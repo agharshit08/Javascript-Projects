73d3f383ec5c57611ef4ffa52ccf55e43e1f350e
Harshit Agrawal <harshitag785@gmail.com>
2019-06-21 17:21:33 +0530

    With Promises

---
+++ Fetch API/easyHttp.js
@@ -0,0 +1,59 @@
+class EasyHTTP {
+
+    // GET Request
+    get(url) {
+        return new Promise((res, rej) => {
+            fetch(url)
+                .then(res => res.json())
+                .then(data => resolve(data))
+                .catch(err => reject(err));
+        });
+    }
+
+    // POST Request
+    post(url, data) {
+        return new Promise((resolve, reject) => {
+            fetch(url, {
+                method: 'POST',
+                headers: {
+                    'Content-type': 'application/json'
+                },
+                body: JSON.stringify(data)
+            })
+                .then(res => res.json())
+                .then(data => resolve(data))
+                .catch(err => reject(err));
+        });
+    }
+
+    // PUT Request
+    put(url, data) {
+        return new Promise((resolve, reject) => {
+            fetch(url, {
+                method: 'PUT',
+                headers: {
+                    'Content-type': 'application/json'
+                },
+                body: JSON.stringify(data)
+            })
+                .then(res => res.json())
+                .then(data => resolve(data))
+                .catch(err => reject(err));
+        });
+    }
+
+    // DELETE Request
+    delete(url){
+        return new Promise((resolve, reject) => {
+            fetch(url, {
+                method: 'DELETE',
+                headers: {
+                    'Content-type': 'application/json'
+                },
+            })
+                .then(res => res.json())
+                .then(data => resolve('Resource Deleted!'))
+                .catch(err => reject(err));
+        });
+    }
+}
